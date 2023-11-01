7afc48c636da55b7dba932aae35aae569d3816eb
Elise Commandre <commandre35@gmail.com>
2023-11-01 19:21:08 +0100

    ajout page welcome

---
+++ welcome.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html >
+<html>
+<head>
+<meta http-equiv="Content-Type"
+content="text/html; charset=UTF-8" />
+<link rel="stylesheet" href="style.css" type="text/css" />
+
+<title>Welcome</title>
+</head>
+	<body>
+			<a href="http://localhost/Projet/inscription.php" class="oval-button">Inscription</a>
+			<a href="http://localhost/Projet/connexion.php" class="oval-button">Connexion</a>
+	</body>
+</html>
