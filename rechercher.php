43688d534da7d0bba50f49ff9cf3a796876c11d9
Elise Commandre <commandre35@gmail.com>
2024-01-25 22:31:32 +0100

    ajout et modifs

--- rechercher.php
+++ rechercher.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "universites";
+$dbname = "projet_universite";
 
 // Créer une connexion à la base de données
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -16,10 +16,11 @@ if ($conn->connect_error) {
 $searchTerm = isset($_GET['q']) ? $conn->real_escape_string($_GET['q']) : "";
 
 // Requête SQL pour rechercher des universités dans la ville spécifiée ou avec le nom spécifié
-$sql = "SELECT u.name AS universite_name, v.name AS ville_name
+$sql = "SELECT u.id_universite, u.name AS universite_name, v.name AS ville_name
         FROM universite u
         LEFT JOIN ville v ON u.id_ville = v.id_ville
         WHERE u.name LIKE '%$searchTerm%' OR v.name LIKE '%$searchTerm%'";
+
 
 $result = $conn->query($sql);
 
@@ -27,7 +28,7 @@ if ($result) {
     // Afficher les résultats
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-            echo "<p>Nom de l'université : " . $row['universite_name'] . " - Ville : " . $row['ville_name'] . "</p>";
+		echo "<p><a href='universite.php?id=" . $row['id_universite'] . "'>" . $row['universite_name'] . "</a> - Ville : " . $row['ville_name'] . "</p>";
         }
     } else {
         echo "Aucun résultat trouvé.";
@@ -40,4 +41,3 @@ if ($result) {
 $conn->close();
 ?>
 
-?>
