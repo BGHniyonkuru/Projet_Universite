fbd2ccc6f88dd42ea7185a78b8e3a0031839b9a8
alvin257 <alviningabire@gmail.com>
2024-02-07 19:38:10 +0100

    mise à jour

--- accueil.php
+++ accueil.php
@@ -8,9 +8,40 @@ session_start();
 		<link rel="stylesheet" href="style.css" type="text/css" />
 
 		<title>Accueil</title>
+		<style>
+			.bandeau{text-decoration: none;
+				color:white;
+				
+			}
+
+			#logo{
+				margin-left:130px;
+				margin-top:10px;
+				height:100px;
+				width:100px;
+			}
+
+			#logo2{
+				margin-left:100px;
+				margin-top:5px;
+				height:50px;
+				width:50px;
+			}
+
+			#logo3{
+				margin-left:10px;
+				margin-top:5px;
+				height:50px;
+				width:50px;
+			}
+			#searchResults{
+				margin-left:5px;
+			}
+	</style>
 	</head>
 <body id="body_accueil">
 	<div class="container">
+
 			<a href= "accueil.php"><img id="logo" src="http://localhost/Projet/images/logo.png" alt="logo" ></a>
 
 			<ul>
@@ -24,7 +55,7 @@ session_start();
 			<a href= "monCompte.php"><img id="logo3" src="http://localhost/Projet/images/monCompte.png" alt="logo"></a>
 		</div>
 	
-<<<<<<< HEAD
+
 		
 		<div>
 			<p id ="titre"> Top 3 2023 best universities</p>
@@ -64,25 +95,4 @@ session_start();
 	</div>
 </body>
 
-=======
-	<body>
-		<!-- bandeau en haut de l'écran -->
-		<div class="container">
-			<a href= "accueil.php"><img id="logo" src="http://localhost/Projet/images/logo.png" alt="logo" ></a>
-
-			<ul>
-			  <li><a class= "bandeau" href="comparer.php">Comparer</a></li>
-			  <li><a class= "bandeau" href="localiser.php">Localiser</a></li>
-			  <li><a class= "bandeau" href="predire.php" >Prédire</a></li>
-			  <li><a class= "bandeau" href="contact.php" >Contact</a></li>
-			  <li><a class= "bandeau" href="search.php" >Rechercher</a></li>
-			</ul>
-			<a href= "compte.php"><img id="logo2" src="http://localhost/Projet/images/favori.png" alt="logo"></a>
-			<a href= "favori.php"><img id="logo3" src="http://localhost/Projet/images/monCompte.png" alt="logo"></a>
-		</div>
-	</body>
-<footer>
-Copyright © 2023 UniDiscover
-</footer>
->>>>>>> 01344d7e8602f68c0eeaaa40305b5b227574c4a9
 </html>
