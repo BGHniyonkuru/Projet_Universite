0e9e8b488ed0a2d6a765087432d9581fe80d6db4
GracePal2 <psidiqa@gmail.com>
2024-04-17 17:32:27 +0200

    ajout fonctionnalite favoris

---
+++ ajouter_aux_favoris.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+
+
+if (isset($_POST['universite_id']) && isset($_POST['client_id'])) {
+    $universite_id = $_POST['universite_id'];
+    $client_id = $_POST['client_id'];
+    $name = $universite_name;
+
+    // Créer une connexion à la base de données
+    require("bd.php");
+    $conn = getBD();
+
+    // Vérifier la connexion
+    if ($conn->connect_error) {
+        die("La connexion a échoué : " . $conn->connect_error);
+    }
+
+    
+    $sql = "INSERT INTO favoris (name, id_universite, id_client) VALUES (:name, :id_universite, :id_client)";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute(
+        array(
+            "name" => $name,
+            "id_universite" => $universite_id,
+            "id_client" => $client_id,
+   
+        )
+    );
+
+    if ($stmt) {
+        echo "Université ajoutée aux favoris.";
+    } else {
+        echo "Erreur : " . $stmt->error;
+    }
+    $stmt->close();
+    $conn->close();
+/*
+    if ($conn->query($sql_insert_favorite) === TRUE) {
+        // Réponse HTTP 200 pour indiquer le succès de l'opération
+        http_response_code(200);
+        echo "L'université a été ajoutée aux favoris avec succès.";
+    } else {
+        // Réponse HTTP 500 pour indiquer une erreur interne du serveur
+        http_response_code(500);
+        echo "Erreur lors de l'ajout de l'université aux favoris : " . $conn->error;
+    }
+
+    // Fermer la connexion à la base de données
+    $conn->close();
+} else {
+    // Réponse HTTP 400 pour indiquer une mauvaise requête
+    http_response_code(400);
+    echo "Paramètres manquants.";
+}*/
+?>
--- universite.php
+++ universite.php
@@ -4,8 +4,9 @@ session_start();
 <!DOCTYPE html >
 <html>
 	<head>
-	<link rel="stylesheet" href="http://localhost/Projet/style_universite.css" type="text/css" />
+	<link rel="stylesheet" href="style_universite.css" type="text/css" />
 	<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+	<script src='https://cdn.jsdelivr.net/npm/chart.js'></script>
 		<title>Universite</title>
 		<style>
 			.bandeau{
@@ -38,25 +39,27 @@ session_start();
 	</head>	
 	<!-- bandeau en haut de l'écran -->
 		<div class="container">
-			<a href= "accueil.php"><img id="logo" src="http://localhost/Projet/images/logo.png" alt="logo" ></a>
+			<a href= "accueil.php"><img id="logo" src="/Projet_universite/images/logo.png" alt="logo" ></a>
 
 			<ul>
-				<li><a class= "bandeau" href="comparer.php">Compare</a></li>
-				<li><a class= "bandeau" href="localiser.php">Map</a></li>
+				<li><a class= "bandeau" href="comparaison.php">Compare</a></li>
+				<li><a class= "bandeau" href="localisation.php">Map</a></li>
 				<li><a class= "bandeau" href="predire.php" >Prédict</a></li>
 				<li><a class= "bandeau" href="contact.php" >Contact</a></li>
 				<li><a class= "bandeau" href="search_university.html" >Search</a></li>
 			</ul>
-			<a href= "favoris.php"><img id="logo2" src="http://localhost/Projet/images/favori.png" alt="logo"></a>
-			<a href= "monCompte.php"><img id="logo3" src="http://localhost/Projet/images/monCompte.png" alt="logo"></a>
+			<a href= "favoris.php"><img id="logo2" src="/Projet_universite/images/favori.png" alt="logo"></a>
+			<a href= "monCompte.php"><img id="logo3" src="/Projet_universite/images/monCompte.png" alt="logo"></a>
 		</div>
 	<body class="page-universite">
 	
 		<?php
+		var_dump($_SESSION['client']);
 		$servername = "localhost";
 		$username = "root";
 		$password = "";
 		$dbname = "projet_universite";
+		$clientId = isset($_SESSION['client']['id']) ? $_SESSION['client']['id'] : null;
 
 		// Créer une connexion à la base de données
 		$conn = new mysqli($servername, $username, $password, $dbname);
@@ -83,8 +86,10 @@ session_start();
 				$description=$row['description'];
 				$description2=$row['description2'];
 				$description3=$row['description3'];
-				echo "<div id='container1' style='background-color: #666666; text-align: center;'><p id='nom_univ' >" . $universite_name . "</p><a href='http://localhost/Projet/search_university.html'><img id='loupe' src='http://localhost/Projet/images/loupe.png' alt='loupe' ></a></div>";
-				echo"<img id='etoile' src='http://localhost/Projet/images/etoile.jpg' alt='etoile' >";
+				echo "<div id='container1' style='background-color: #666666; text-align: center;'><p id='nom_univ' >" . $universite_name . "</p><a href='http://localhost/Projet_universite/search.html'><img id='loupe' src='/Projet_universite/images/loupe.png' alt='loupe' ></a></div>";
+				echo "<button type='button' id='favori' style='border: none; background: none; cursor: pointer; float: right; margin-top: 10px;'>
+						<img src='/Projet_universite/images/etoile.jpg' alt='etoile' style='height: 30px; width: 30px;'></button>";  
+
 				if (!empty($row['image'])) {
 					// Affichez l'image en utilisant la balise <img>
 					echo "<img src='" . $row['image'] . "' alt='Image de l\'université' style='width: 10%; float: left; margin-left: 5%;margin-top:40px;'>";
@@ -129,9 +134,9 @@ session_start();
 			// Ajouter le script Plotly pour générer le graphique
 			echo "<div style='height: 30px;'></div>
 			<canvas id='myChart'  width='50' height='15' ></canvas>
-			<div style='height: 50px;'></div>
-			<script src='https://cdn.jsdelivr.net/npm/chart.js'></script>
-			<script>
+			<div style='height: 50px;'></div>";
+			
+			echo"<script>
 				var ctx = document.getElementById('myChart').getContext('2d');
 				var myChart = new Chart(ctx, {
 					type: 'line',
@@ -178,7 +183,7 @@ session_start();
 						}
 					}
 				});
-				
+					
 			</script>";
 
 		} else {
@@ -188,6 +193,29 @@ session_start();
 		// Fermer la connexion à la base de données
 		$conn->close();
 		?>
+		<script>
+			// Ajout de l'écouteur d'événement pour l'icône étoile
+		document.getElementById('etoile').addEventListener('click', function() {
+			ajouterAuxFavoris();
+		});
+
+		function ajouterAuxFavoris() {
+			var idUniversite = $universite_id; // Mettez ici l'ID de l'université, normalement passé dynamiquement
+			var client_id= $_SESSION["client"]["id_client"];
+			var name = $universite_name;
+			$.ajax({
+				url: 'ajouter_aux_favoris.php',  // Assurez-vous que ce fichier existe et est accessible
+				type: 'POST',
+				data: { idUniversite: idUniversite },
+				success: function(response) {
+					alert('Ajouté aux favoris! Réponse du serveur : ' + response);
+				},
+				error: function() {
+					alert('Erreur lors de l\'ajout aux favoris.');
+				}
+			});
+		}
+		</script>
 	</body>
 	<footer>
 		Copyright © 2023 UniDiscover
